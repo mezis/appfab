dea5954720e9f52c17f786523b9c0d5cb650125e
Julien Letessier <julien.letessier@gmail.com>
2012-10-31 15:50:58 +0000

    User role helpers

--- app/models/user_role.rb
+++ app/models/user_role.rb
@@ -1,11 +1,46 @@
 class UserRole < ActiveRecord::Base
-  Names = %w(product_manager architect developer benevolent_dictator account_owner)
+  Names = [:product_manager, :architect, :developer, :benevolent_dictator, :account_owner]
 
-  attr_accessible :user, :name
+  attr_accessible :user, :name, :user_id
 
   belongs_to :user
   validates_presence_of   :user
   validates_presence_of   :name
   validates_inclusion_of  :name, in: Names
   validates_uniqueness_of :name, scope: :user_id
+
+  # User methods, defined here to separate concerns
+  module UserMethods
+    def self.included(by)
+      by.class_eval do
+        has_many :roles, class_name: 'UserRole'
+
+        scope :playing, lambda { |*role_names|
+          joins(:roles).where('user_roles.name' => role_names)
+        }
+      end
+    end
+
+    def plays!(role_name)
+      self.roles.transaction do
+        return if plays?(role_name)
+        self.roles.create!(name: role_name)
+      end
+      return self
+    end
+
+    def plays?(role_name)
+      role_name = role_name.to_sym if role_name.kind_of?(String)
+      Names.include?(role_name) or
+        raise ArgumentError.new("Unknown role '#{role_name}'")
+      return self.roles.where(name: role_name).any?
+    end
+  end
+
+  private
+
+  before_validation do |record|
+    record.name = record.name.to_sym if record.name.kind_of?(String)
+  end
+
 end
--- spec/models/user_role_spec.rb
+++ spec/models/user_role_spec.rb
@@ -7,7 +7,56 @@ describe UserRole do
 
   it 'does not allow duplicates' do
     @user = User.make!
-    @user.roles.create!(name:'developer')
-    @user.roles.build(name:'developer').should_not be_valid
+    @user.roles.create!(name: :developer)
+    @user.roles.build(name: :developer).should_not be_valid
+  end
+end
+
+describe UserRole::UserMethods do
+  describe '#plays?' do
+    before do
+      @user = User.make!
+      @user.roles.create!(name: :developer)
+    end
+
+    it 'tests for the named role' do
+      @user.plays?(:developer).should be_true
+      @user.plays?(:product_manager).should be_false
+    end
+
+    it 'accepts strings' do
+      @user.plays?('developer').should be_true
+    end
+
+    it 'fails on unknown roles' do
+      expect {
+        @user.plays?(:foobar)
+      }.to raise_error(ArgumentError)
+    end
+  end
+
+  describe '#plays!' do
+    it 'adds the role to the user' do
+      @user = User.make!
+      @user.plays!(:developer)
+      @user.roles.should_not be_empty
+      @user.roles.first.name.should == 'developer'
+    end
+
+    it 'is idempotent' do
+      @user = User.make!
+      @user.plays!(:developer)
+      @user.plays!(:developer)
+      @user.plays?(:developer).should be_true
+    end
+  end
+
+  describe '.playing' do
+    it 'returns users with the given roles' do
+      User.make!.plays!(:developer)
+      User.make!.plays!(:product_manager)
+      User.playing(:developer).count.should == 1
+      User.playing(:developer, :product_manager).count.should == 2
+    end
   end
 end
