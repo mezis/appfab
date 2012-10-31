cc72e1b597e154cd4daadcaa681d042950f991f0
Julien Letessier <julien.letessier@gmail.com>
2012-10-31 16:19:41 +0000

    Production OAuth2 settings

--- config/initializers/_settings.rb
+++ config/initializers/_settings.rb
@@ -1,10 +1,12 @@
 require 'configatron'
 
-configatron.omniauth.google_oauth2.app_id = '829784005951.apps.googleusercontent.com'
-configatron.omniauth.google_oauth2.app_secret = 'pxnPqfWV2m6DHg4XygEhOYlW'
-
 configatron.socialp.default_karma = 10
 
 if Rails.env.production?
   # production-specific settings
+  configatron.omniauth.google_oauth2.app_id     = '829784005951-octuu39kre09vnkg44uen7jvr4o96rnd.apps.googleusercontent.com'
+  configatron.omniauth.google_oauth2.app_secret = 'fW_q7ngOBH2ptMhpp4dvP-Rr'
+else
+  configatron.omniauth.google_oauth2.app_id     = '829784005951.apps.googleusercontent.com'
+  configatron.omniauth.google_oauth2.app_secret = 'pxnPqfWV2m6DHg4XygEhOYlW'
 end
