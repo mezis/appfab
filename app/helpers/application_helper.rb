301d67479dd0428f6e65c85c9cd65b29c2f0a18c
Julien Letessier <julien.letessier@gmail.com>
2012-11-03 12:44:57 +0000

    Adds karma symbol helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -8,5 +8,8 @@ module ApplicationHelper
 
   def tshirt_size_options
     options_for_select([['XS', 1], ['S', 2], ['M', 3], ['L', 4]]) 
+
+  def karma_symbol
+    content_tag(:i, '', :class => 'icon-leaf')
   end
 end
