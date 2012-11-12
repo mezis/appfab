31e71fb398657ec205f75fea188916a05f7c081d
Julien Letessier <julien.letessier@gmail.com>
2012-11-12 00:29:34 +0000

    Fixes header when logged out

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,14 +3,19 @@ module ApplicationHelper
 
   def content_tag_with_path_checking(tag, options = {})
     html_options = options.delete(:html) || {}
-    options_to_match = options.reverse_merge(controller: 'ideas', action: 'index')
-    is_current = (params.symbolize_keys.slice(*options_to_match.keys) == options_to_match)
-    path = url_for(options_to_match)
 
-    if is_current
-      classes = html_options.fetch(:class, '').split
-      classes.push 'active'
-      options[:class] = classes.join(' ')
+    if current_user
+      options_to_match = options.reverse_merge(controller: 'ideas', action: 'index')
+      is_current = (params.symbolize_keys.slice(*options_to_match.keys) == options_to_match)
+      path = url_for(options_to_match)
+
+      if is_current
+        classes = html_options.fetch(:class, '').split
+        classes.push 'active'
+        options[:class] = classes.join(' ')
+      end
+    else
+      path = '#'
     end
 
     content_tag(tag, options) do
