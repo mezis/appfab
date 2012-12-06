f66c9e466d9122b9dde55a8dd5b3c6dde4875ebb
Julien Letessier <julien.letessier@gmail.com>
2012-12-06 08:55:30 +0000

    Adds idea lifecycle authorisation

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -1,8 +1,13 @@
 module AuthorizationHelper
   def not_authorized_message(action, object)
     case action
-    when :update
-      _('Sorry, you may not update this %{object}') % { object: _(object.class.name.downcase) }
+    when :update, :vet, :destroy, :edit, :pick
+      _('Sorry, you may not %{verb} this %{object}.') % { verb: action, object: _(object.class.name.downcase) }
+    when :vote
+      _('Sorry, you may not vote for this %{object}.') % { object: _(object.class.name.downcase) }
+    else
+      require 'pry' ; require 'pry-nav' ; binding.pry
+      raise ArgumentError
     end
   end
 end
--- app/models/ability.rb
+++ app/models/ability.rb
@@ -28,27 +28,19 @@ class Ability
     can :create,  Vote, subject_type: 'Idea'
     can :destroy, Vote.recently_created
 
-    # Define abilities for the passed in user here. For example:
-    #
-    #   user ||= User.new # guest user (not logged in)
-    #   if user.admin?
-    #     can :manage, :all
-    #   else
-    #     can :read, :all
-    #   end
-    #
-    # The first argument to `can` is the action you are giving the user permission to do.
-    # If you pass :manage it will apply to every action. Other common actions here are
-    # :read, :create, :update and :destroy.
-    #
-    # The second argument is the resource the user can perform the action on. If you pass
-    # :all it will apply to every resource. Otherwise pass a Ruby class of the resource.
-    #
-    # The third argument is an optional hash of conditions to further filter the objects.
-    # For example, here the user can only update published articles.
-    #
-    #   can :update, Article, :published => true
-    #
-    # See the wiki for details: https://github.com/ryanb/cancan/wiki/Defining-Abilities
+    # Idea lifecycle
+    if user.plays?(:product_manager)
+      can :pick, Idea
+    end
+    can :design,    Idea, product_manager_id: user.id
+    can :implement, Idea, product_manager_id: user.id
+    can :deliver,   Idea, product_manager_id: user.id
+
+
+    if user.plays?(:benevolent_dictator)
+      can :approve,  Idea
+      can :sign_off, Idea
+      can :veto,     Idea
+    end
   end
 end
