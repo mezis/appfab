c6b55f314e619d3556a04bf67e867b81c701a070
Julien Letessier <julien.letessier@gmail.com>
2012-12-07 15:58:21 +0000

    Adds header counters

---
+++ app/helpers/header_helper.rb
@@ -0,0 +1,9 @@
+module HeaderHelper
+
+  def header_angle_counter(angle)
+    return unless current_account
+    count = current_account.ideas.send(:"#{angle}_by", current_user).count
+    return if count == 0
+    content_tag(:div, count, class:'badge badge-small')
+  end
+end
