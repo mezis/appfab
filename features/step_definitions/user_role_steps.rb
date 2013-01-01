2f35007208b1a98c92b33303a12e3fb261627abd
Julien Letessier <julien.letessier@gmail.com>
2013-01-01 23:33:58 +0000

    Cleans up user role cuke steps

--- features/step_definitions/user_role_steps.rb
+++ features/step_definitions/user_role_steps.rb
@@ -1,18 +1,17 @@
+# actions
 
 When /^I (give|remove) the "(.*?)" role (?:to|from) "(.*?)"$/ do |give_or_remove, role_name, first_name|
   user = User.first_name_is(first_name).last
 
   visit "/users/#{user.id}"
-  # require 'pry' ; require 'pry-nav' ; binding.pry
   click_on role_name.capitalize
-  # pending # express the regexp above with the code you wish you had
 end
+
+#  expectations
 
 Then /^"(.*?)" should( not)? be a (.*)$/ do |first_name, negate, role_name|
   user = User.first_name_is(first_name).last
   visit "/users/#{user.id}"
   page.send (negate ? :should_not : :should),
     have_selector('.roles', text: Regexp.new(role_name))
-  # click_on role_name.capitalize
-  # pending # express the regexp above with the code you wish you had
 end
