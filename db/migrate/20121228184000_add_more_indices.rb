2949037c98849aaff9f8c0e2ec1d32d323d6fd8c
Julien Letessier <julien.letessier@gmail.com>
2013-01-01 23:33:21 +0000

    Improves idea indices

--- db/migrate/20121228184000_add_more_indices.rb
+++ db/migrate/20121228184000_add_more_indices.rb
@@ -1,9 +1,13 @@
 class AddMoreIndices < ActiveRecord::Migration
   def up
-    add_index :ideas, [:account_id, :state]
+    add_index :ideas, [:account_id, :state, :active_at]
+    remove_index :ideas, :state
+    add_index :users, :login_id
   end
 
   def down
-    remove_index :ideas, [:account_id, :state]
+    remove_index :users, :login_id
+    add_index :ideas, :state
+    remove_index :ideas, [:account_id, :state, :active_at]
   end
 end
