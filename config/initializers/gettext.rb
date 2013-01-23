46bf3b53456200197113ded528a907ef9cb9f14f
Julien Letessier <julien.letessier@gmail.com>
2013-01-23 14:16:39 +0000

    Fixes i18n setup (list of locales)

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -4,5 +4,6 @@ require 'gettext_i18n_rails/string_interpolate_fix'
 
 Object.send(:include, FastGettext::Translation)
 
+FastGettext.available_locales = [:en]
 FastGettext.add_text_domain('app_fab', :path => 'config/locales')
 FastGettext.default_text_domain = 'app_fab'
