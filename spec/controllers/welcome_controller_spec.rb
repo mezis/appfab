be76d4191c0986928538e5a761ca64fa56d7e276
Julien Letessier <julien.letessier@gmail.com>
2013-01-30 16:20:58 +0000

    Fixes pending specs for static pages

--- spec/controllers/welcome_controller_spec.rb
+++ spec/controllers/welcome_controller_spec.rb
@@ -3,16 +3,30 @@ require 'spec_helper'
 
 describe WelcomeController do
 
-  describe "GET 'index'" do
+  describe "#index" do
     it "returns http success" do
-      get 'index'
+      get :index
       response.should be_success
     end
   end
 
   describe '#static_page' do
-    it 'returns not found for unknown pages'
-    it 'shows content for known pages'
+    around do |example|
+      described_class.with_constants :STATIC_PAGES => %w(foo-bar hello-world) do
+        example.run
+      end
+    end
+
+    it 'returns not found for unknown pages' do
+      get :static_page, page:'unknown'
+      response.should be_not_found
+    end
+
+    it 'shows content for known pages' do
+      controller.should_receive(:render, with:'foo_bar').any_number_of_times
+      get :static_page, page:'foo-bar'
+      response.should be_success
+    end
   end
 
 end
