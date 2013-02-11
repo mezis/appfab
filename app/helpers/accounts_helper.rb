08106401e6055a4eb85188ffa2796d9c9684bf17
Julien Letessier <julien.letessier@gmail.com>
2013-02-11 15:42:12 +0000

    Fixes category filter menu

--- app/helpers/accounts_helper.rb
+++ app/helpers/accounts_helper.rb
@@ -2,6 +2,17 @@
 module AccountsHelper
 
   def account_categories(user)
-    user.andand.account.andand.categories || []
+    user.andand.account.andand.categories.to_a || []
+  end
+
+  def account_category_title(category)
+    case category
+    when 'all'
+      _('All categories')
+    when 'none'
+      _('No category')
+    else
+      category
+    end
   end
 end
