68924286ea2e660e0ac547b5c72198a3908ef550
Julien Letessier <julien.letessier@gmail.com>
2013-02-16 23:25:46 +0000

    Fixes Haml setup

--- config/initializers/haml_redcarpet.rb
+++ config/initializers/haml_redcarpet.rb
@@ -1,9 +1,9 @@
 # encoding: utf-8
+require "redcarpet"
 
 # Adds Redcarpet support to Haml's :markdown filter.
 module Haml::Filters::Markdown
   include Haml::Filters::Base
-  lazy_require "redcarpet"
 
   def render(text)
     Object.new.extend(PipelineHelper).pipeline_render(text)
