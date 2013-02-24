09d7ab2535ca352411e868f301b100daa5ac03e5
Julien Letessier <julien.letessier@gmail.com>
2013-02-24 21:32:04 +0000

    Fixes Sentry configuration

--- config/initializers/sentry-raven.rb
+++ config/initializers/sentry-raven.rb
@@ -1,13 +1,13 @@
 RAVEN_URLS = {
-  staging:    'https://8349a0b5980a461c8552e1ca0b39e00a:c1d41bb8cfd240479dbeaed0078cd424@app.getsentry.com/5334',
-  production: 'https://e63cfbdf8e38470fb34511dfe06b6b36:bcc94f320b54470094fff03c09b393b7@app.getsentry.com/5355'
+  'staging'    => 'https://8349a0b5980a461c8552e1ca0b39e00a:c1d41bb8cfd240479dbeaed0078cd424@app.getsentry.com/5334',
+  'production' => 'https://e63cfbdf8e38470fb34511dfe06b6b36:bcc94f320b54470094fff03c09b393b7@app.getsentry.com/5355'
 }
 
-if RAVEN_URLS[Rails.env.to_sym]
+if RAVEN_URLS[Rails.env]
   require 'raven'
 
   Raven.configure do |config|
+    config.dsn = RAVEN_URLS[Rails.env]
     config.environments = RAVEN_URLS.keys
-    config.dsn = RAVEN_URLS[Rails.env.to_sym]
   end
 end
