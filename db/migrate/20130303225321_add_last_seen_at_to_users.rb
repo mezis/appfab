8c82d8a480b1abec551f328e550af985feebcfa9
Julien Letessier <julien.letessier@gmail.com>
2013-03-04 00:03:16 +0000

    Fixes incompatible migration

--- db/migrate/20130303225321_add_last_seen_at_to_users.rb
+++ db/migrate/20130303225321_add_last_seen_at_to_users.rb
@@ -1,12 +1,6 @@
 class AddLastSeenAtToUsers < ActiveRecord::Migration
   def up
     add_column :users, :last_seen_at, :datetime
-    update %Q{
-      UPDATE users
-      INNER JOIN logins
-      ON users.login_id = logins.id
-      SET users.last_seen_at = logins.last_sign_in_at
-    }
   end
 
   def down
