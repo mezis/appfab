69fc78ea8092b78c98d62909a771b0d8887acc95
Julien Letessier <julien.letessier@gmail.com>
2013-03-05 14:43:00 +0000

    Removes leftover from comment tree

--- app/models/idea.rb
+++ app/models/idea.rb
@@ -9,7 +9,6 @@ class Idea < ActiveRecord::Base
   has_many   :vettings, :dependent => :destroy
   has_many   :votes, :as => :subject, :dependent => :destroy
   has_many   :comments, :dependent => :destroy
-  has_many   :toplevel_comments, :class_name => 'Comment', :as => :parent
   has_many   :attachments, :class_name => 'Attachment', :as => :owner, :dependent => :destroy
   belongs_to :product_manager, :class_name => 'User'
 
