be1cb8a739f91625bcaa5cec9a7ae309cd15d502
Julien Letessier <julien.letessier@gmail.com>
2013-03-16 23:19:59 +0000

    Turns "traits" into "concerns"

--- app/controllers/traits/render_ujs.rb
+++ app/controllers/traits/render_ujs.rb
@@ -1,5 +1,7 @@
 # Exposes +render_ujs+.
 module Traits::RenderUjs
+  extend ActiveSupport::Concern
+
   protected 
   def render_ujs(record_or_collection, options={})
     render partial:'ujs', locals: { content:record_or_collection, options:options }
--- app/models/traits/recent_creation.rb
+++ app/models/traits/recent_creation.rb
@@ -1,11 +1,11 @@
 module Traits::RecentCreation
+  extend ActiveSupport::Concern
+
   RECENT_CREATION_DELAY = 15.minutes
 
-  def self.included(by)
-    by.class_eval do
-      scope :recently_created, lambda { where('created_at > ?', RECENT_CREATION_DELAY.ago) }
-      scope :recently_updated, lambda { where('updated_at > ?', RECENT_CREATION_DELAY.ago) }
-    end
+  included do
+    scope :recently_created, lambda { where('created_at > ?', RECENT_CREATION_DELAY.ago) }
+    scope :recently_updated, lambda { where('updated_at > ?', RECENT_CREATION_DELAY.ago) }
   end
 
   def recently_created?
