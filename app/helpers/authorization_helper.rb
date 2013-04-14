a43edf33eb528c82bf59c7bd359e2968c0b9d734
Julien Letessier <julien.letessier@gmail.com>
2013-04-14 16:13:41 +0100

    Renames vote to endorse in the UI. Fixes #53.

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -4,11 +4,10 @@ module AuthorizationHelper
     when :update, :vet, :destroy, :edit, :pick
       _('Sorry, you may not %{verb} this %{object}.') % { verb: action, object: _(object.class.name.downcase) }
     when :vote
-      _('Sorry, you may not vote for this %{object}.') % { object: _(object.class.name.downcase) }
+      _('Sorry, you may not endorse or vote for this %{object}.') % { object: _(object.class.name.downcase) }
     when :create
       _('Sorry, you may not create a %{class}.') % { class: _(object.name.downcase) }
     else
-      require 'pry' ; require 'pry-nav' ; binding.pry
       raise ArgumentError
     end
   end
