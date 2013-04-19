4bdc2e49d6f8254c77160d278810f530bbdf9874
Julien Letessier <julien.letessier@gmail.com>
2013-04-19 15:10:18 +0100

    Adds automatic per-page stylesheets

--- app/helpers/layouts_helper.rb
+++ app/helpers/layouts_helper.rb
@@ -29,4 +29,15 @@ module LayoutsHelper
   def body_id
     "#{params[:controller]}-#{params[:action]}".gsub(/\W/, '-').gsub(/--+/,'-')
   end
+
+  def page_stylesheet_tag
+    controller = params[:controller]
+    @page_stylesheet_tags ||= {}
+    @page_stylesheet_tags[controller] ||= begin
+      css_path = Pathname.pwd.
+        join('app/assets/stylesheets/pages').
+        join("#{controller}.css.sass")
+      css_path.exist? ? stylesheet_link_tag("pages/#{controller}") : ''
+    end
+  end
 end
