7a1e6f71b2550ce0eed246b27e45776dd8a98f7b
Julien Letessier <julien.letessier@gmail.com>
2013-04-21 15:49:33 +0100

    Improves authorization error messages

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -5,8 +5,8 @@ module AuthorizationHelper
       _('Sorry, you may not %{verb} this %{object}.') % { verb: action, object: _(object.class.name.downcase) }
     when :vote
       _('Sorry, you may not vote for this %{object}.') % { object: _(object.class.name.downcase) }
-    when :create
-      _('Sorry, you may not create a %{class}.') % { class: _(object.name.downcase) }
+    when :create, :new
+      _('Sorry, you may not create %{class}.') % { class: _(object.name.downcase.pluralize) }
     else
       require 'pry' ; require 'pry-nav' ; binding.pry
       raise ArgumentError
