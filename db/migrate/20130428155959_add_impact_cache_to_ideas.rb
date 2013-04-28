240daa8ecb75bb8aed9c258ab72fc92eb65e1f60
Julien Letessier <julien.letessier@gmail.com>
2013-04-29 00:19:45 +0100

    Fixes migration to keep PG happy

--- db/migrate/20130428155959_add_impact_cache_to_ideas.rb
+++ db/migrate/20130428155959_add_impact_cache_to_ideas.rb
@@ -6,51 +6,40 @@ class AddImpactCacheToIdeas < ActiveRecord::Migration
     add_column :comments, :votes_cache,  :integer, :limit => 2, :default => 0
 
     update %Q{
-      UPDATE ideas SET ideas.impact_cache = NULL
-    }
+      UPDATE #{qtn 'ideas'} SET impact_cache = NULL
+    }.gsub(/\s+/, ' ')
 
     update %Q{
-      UPDATE ideas
-      SET ideas.impact_cache = 
-        1000 * ideas.rating / (ideas.design_size + ideas.development_size)
+      UPDATE #{qtn 'ideas'}
+      SET impact_cache =
+        1000 * #{qtn 'ideas'}.rating / (#{qtn 'ideas'}.design_size + #{qtn 'ideas'}.development_size)
       WHERE TRUE
       AND ideas.rating           IS NOT NULL
       AND ideas.design_size      IS NOT NULL
       AND ideas.development_size IS NOT NULL      
-    }
-
-    update %Q{
-      UPDATE ideas
-      LEFT JOIN (
-        SELECT 
-          votes.subject_id AS idea_id,
-          count(*) AS vote_count
-        FROM votes
-        WHERE votes.subject_type = 'Idea'
-        GROUP BY votes.subject_id
-      ) AS votes_aggregate
-      ON
-        ideas.id = votes_aggregate.idea_id
-      SET
-        ideas.votes_cache = votes_aggregate.vote_count
     }.gsub(/\s+/, ' ')
 
 
-    update %Q{
-      UPDATE comments
-      LEFT JOIN (
-        SELECT 
-          votes.subject_id AS comment_id,
-          count(*) AS vote_count
+    %w(idea comment).each do |model|
+      t = qtn model.pluralize
+      m = model.titleize
+
+      vote_counts = select_rows %Q{
+        SELECT votes.subject_id AS idea_id,
+          count(1) AS vote_count
         FROM votes
-        WHERE votes.subject_type = 'Comment'
+        WHERE votes.subject_type = '#{m}'
         GROUP BY votes.subject_id
-      ) AS votes_aggregate
-      ON
-        comments.id = votes_aggregate.comment_id
-      SET
-        comments.votes_cache = votes_aggregate.vote_count
-    }.gsub(/\s+/, ' ')
+      }.gsub(/\s+/, ' ')
+
+      vote_counts.each do |id, count|
+        update %Q{
+          UPDATE #{t}
+          SET votes_cache = #{count}
+          WHERE id = #{id}
+        }.gsub(/\s+/, ' ')
+      end
+    end
   end
 
   def down
@@ -58,5 +47,15 @@ class AddImpactCacheToIdeas < ActiveRecord::Migration
     remove_column :ideas,    :stars_cache
     remove_column :ideas,    :votes_cache
     remove_column :comments, :votes_cache
+  end
+
+  private
+
+  def qtn(*args)
+    ActiveRecord::Base.connection.quote_table_name(*args)
+  end
+
+  def qcn(*args)
+    ActiveRecord::Base.connection.quote_column_name(*args)
   end
 end
