659dc486e3a0100161a7e1e2d260ff55dac5413d
Julien Letessier <julien.letessier@gmail.com>
2013-06-09 17:50:40 +0100

    Fixes dashboad for new users

--- app/controllers/dashboards_controller.rb
+++ app/controllers/dashboards_controller.rb
@@ -1,6 +1,8 @@
 class DashboardsController < ApplicationController
   include Traits::RequiresLogin
 
+  before_filter :require_account!
+
   def show
     @dashboard = Dashboard.new user:current_user
   end
