39a19e04239fcedef443ad6055644e4baae3b26c
Stefan Dorunga <sdorunga@housetrip.com>
2013-07-27 13:02:36 +0100

    Remove double semicolons from class names

--- app/helpers/authorization_helper.rb
+++ app/helpers/authorization_helper.rb
@@ -2,16 +2,16 @@ module AuthorizationHelper
   def not_authorized_message(action, object)
     case action
     when :update, :vet, :destroy, :edit, :pick
-      _('Sorry, you may not %{verb} this %{object}.') % { verb: action, object: _(object.class.name.downcase) }
+      _('Sorry, you may not %{verb} this %{object}.') % { verb: action, object: _(object.class.name.gsub('::', ' ').downcase) }
     when :update_admin
-      _('Sorry, you may not %{verb} this %{object}.') % { verb: 'update', object: _(object.class.name.downcase) }
+      _('Sorry, you may not %{verb} this %{object}.') % { verb: 'update', object: _(object.class.name.gsub('::', ' ').downcase) }
     when :vote
       ( object.kind_of?(Idea) ?
         _('Sorry, you may not endorse this %{object}.') :
         _('Sorry, you may not vote for this %{object}.')
-      ) % { object: _(object.class.name.downcase) }
+      ) % { object: _(object.class.name.gsub('::', ' ').downcase) }
     when :create, :new
-      _('Sorry, you may not create %{class}.') % { class: _(object.name.downcase.pluralize) }
+      _('Sorry, you may not create %{class}.') % { class: _(object.name.gsub('::', ' ').downcase.pluralize) }
     else
       raise ArgumentError
       raise ArgumentError, 'unknown action'
