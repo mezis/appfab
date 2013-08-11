c9f9f5f1254d22bfaa31c53bff838680cced638f
Julien Letessier <julien.letessier@gmail.com>
2013-08-11 17:58:43 +0100

    Adds counter caches for attachment counts

--- app/models/attachment.rb
+++ app/models/attachment.rb
@@ -1,7 +1,7 @@
 class Attachment < ActiveRecord::Base
   attr_accessible :mime_type, :size, :name, :owner, :uploader, :file
 
-  belongs_to :owner, :polymorphic => true
+  belongs_to :owner, :polymorphic => true, :counter_cache => true
   belongs_to :uploader, :class_name => 'User'
 
   file_accessor :file
