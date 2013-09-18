d22ae0a9f3a0b201453c4c40263a02ae50dc4e4d
Julien Letessier <julien.letessier@gmail.com>
2013-09-18 22:14:07 +0100

    Adds invites controller

---
+++ app/controllers/user/invites_controller.rb
@@ -0,0 +1,21 @@
+require 'user_invitation_service'
+
+class User::InvitesController < ApplicationController
+  before_filter :authenticate_login!
+
+  def create
+    invitee = Login.new params[:login].slice(:email, :first_name, :last_name)
+    @invited = UserInvitationService.new(inviter:current_user, login:invitee).run
+
+    if @invited
+      flash[:success] = _("Invite sent to %{user}") % { user: invitee.first_name }
+    else
+      flash[:notice] = _("No need, %{user} is already a member!") % { user: invitee.first_name }
+    end
+
+    respond_to do |format|
+      format.html { redirect_to :back }
+      format.js
+    end
+  end
+end
