42cfc491d8ac174e767102ae5d9710926bee0135
Julien Letessier <julien.letessier@gmail.com>
2013-12-13 18:05:09 +0000

    Patches ActiveSupport::SafeBuffer for string interpolation

--- config/initializers/gettext.rb
+++ config/initializers/gettext.rb
@@ -1,9 +1,24 @@
 # encoding: UTF-8
 require 'fast_gettext'
-require 'gettext_i18n_rails/string_interpolate_fix'
 
 Object.send(:include, FastGettext::Translation)
 
 FastGettext.available_locales = [:en]
 FastGettext.add_text_domain('app_fab', :path => 'config/locales')
 FastGettext.default_text_domain = 'app_fab'
+
+# TODO: this can go away when the following PR is released to Rails
+# https://github.com/rails/rails/pull/13310
+ActiveSupport::SafeBuffer.class_eval do
+  def %(args)
+    escaper = ->(arg) { (!html_safe? || arg.html_safe?) ? arg : ERB::Util.h(arg) }
+    case args
+    when Hash
+      escaped_args = Hash[args.map { |k,arg| [k, escaper.call(arg)] }]
+    else
+      escaped_args = Array(args).map { |arg| escaper.call(arg) }
+    end
+
+    self.class.new(super(escaped_args))
+  end
+end
