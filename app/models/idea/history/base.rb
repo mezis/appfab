14272bc4dccafa90ed829c2aa18d6527d007db7e
Julien Letessier <julien.letessier@gmail.com>
2013-12-30 15:24:17 +0000

    Fixes broken associations

--- app/models/idea/history/base.rb
+++ app/models/idea/history/base.rb
@@ -2,7 +2,7 @@ class Idea::History::Base < ActiveRecord::Base
   self.table_name = 'idea_histories'
   # attr_accessible :idea, :subject
 
-  belongs_to :idea
+  belongs_to :idea, inverse_of: :histories
   belongs_to :subject, polymorphic:true
 
   scope :by_created_at,  -> { order('created_at DESC') }
@@ -13,7 +13,7 @@ class Idea::History::Base < ActiveRecord::Base
     extend ActiveSupport::Concern
 
     included do
-      has_many :histories, :class_name => 'Idea::History::Base', :dependent => :destroy
+      has_many :histories, :class_name => 'Idea::History::Base', :dependent => :destroy, inverse_of: :idea
     end
   end
 end
--- app/models/idea/history/comment.rb
+++ app/models/idea/history/comment.rb
@@ -10,7 +10,7 @@ class Idea::History::Comment < Idea::History::Base
     extend ActiveSupport::Concern
     
     included do
-      has_one :idea_history, dependent: :destroy, class_name:'Idea::History::Comment', as: :subject
+      has_one :idea_history, dependent: :destroy, class_name:'Idea::History::Comment', as: :subject, inverse_of: :subject
       after_create do |record|
         record.create_idea_history!(idea:record.idea)
       end
