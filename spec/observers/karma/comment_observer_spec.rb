32b0987d2b881941f8249975778996ba566ba874
Julien Letessier <julien.letessier@gmail.com>
2013-12-31 14:01:29 +0000

    Fixes flakey spec

--- spec/observers/karma/comment_observer_spec.rb
+++ spec/observers/karma/comment_observer_spec.rb
@@ -3,13 +3,14 @@ require 'spec_helper'
 
 describe Karma::CommentObserver do
   context 'when a comment is posted' do
-    let(:author)  { User.make! }
-    let(:idea)    { Idea.make! }
-    let(:comment) { Comment.make! author: author, idea: idea }
+    fixtures :ideas, :users, :accounts, :logins
 
     it 'the commenter gains karma' do
-      idea # make sure the idea is created outside the 'should' block
-      lambda { comment }.should change(author, :karma).by(1)
+      idea   = ideas(:idea_submitted)
+      author = User.make!
+      lambda {
+        Comment.make! author: author, idea: idea
+      }.should change(author, :karma).by(1)
     end
   end
 end
